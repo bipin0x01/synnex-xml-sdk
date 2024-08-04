b093dc52199bcaf37c8e060c00804c293306cb09
Bipin Thapa <bt.kaji@gmail.com>
2024-08-04 12:38:08 +0545

    change response casings

--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -1,4 +1,23 @@
 import { parseStringPromise } from "xml2js";
+import _ from "lodash";
+
+// Function to recursively transform object keys to camel case
+const keysToCamel = (obj: any): any => {
+  if (_.isArray(obj)) {
+    return obj.map((v) => keysToCamel(v));
+  } else if (_.isObject(obj)) {
+    return _.reduce(
+      obj,
+      (result, value, key) => {
+        const camelKey = _.camelCase(key);
+        result[camelKey] = keysToCamel(value);
+        return result;
+      },
+      {} as any
+    );
+  }
+  return obj;
+};
 
 export const parseXmlToJson = async (xml: string): Promise<any> => {
   try {
@@ -7,11 +26,12 @@ export const parseXmlToJson = async (xml: string): Promise<any> => {
       explicitArray: false, // Prevent wrapping single values in arrays
       mergeAttrs: true, // Merge attributes with child elements
       trim: true, // Trim whitespace around text nodes
-      normalizeTags: true, // Convert tags to lowercase
+      normalizeTags: false, // Convert tags to lowercase
       explicitRoot: false, // Remove the root element wrapper
     };
 
-    return await parseStringPromise(xml, options);
+    const jsonResult = await parseStringPromise(xml, options);
+    return keysToCamel(jsonResult);
   } catch (error: any) {
     throw new Error("Failed to parse XML: " + error.message);
   }
